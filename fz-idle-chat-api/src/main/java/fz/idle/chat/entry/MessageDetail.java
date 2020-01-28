254e0ded840d451a79cf310222edd9115f5d78fd
fanke@netposa.com <fanke@netposa.com>
2020-01-28 12:45:54 +0800

    增加字段注释

--- fz-idle-chat-api/src/main/java/fz/idle/chat/entry/MessageDetail.java
+++ fz-idle-chat-api/src/main/java/fz/idle/chat/entry/MessageDetail.java
@@ -1,14 +1,18 @@
 package fz.idle.chat.entry;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
-
 @Data
 public class MessageDetail implements Serializable {
+    @ApiModelProperty(hidden = true)
     private static final long serialVersionUID = 6297604862067678805L;
+    @ApiModelProperty("用户id")
     private String clientId;
+    @ApiModelProperty("好友id")
     private String friendId;
+    @ApiModelProperty("消息内容")
     private String detail;
 
 }
