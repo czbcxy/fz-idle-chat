a9740df40817c5d1aa20a6c575b60bc2af3394e8
czbcxy <k995211314@163.com>
2020-01-29 21:08:02 +0800

    update

--- fz-idle-chat-consumer/src/main/java/fz/idle/chat/serverImpl/ChatConsumer.java
+++ fz-idle-chat-consumer/src/main/java/fz/idle/chat/serverImpl/ChatConsumer.java
@@ -1,27 +1,17 @@
 package fz.idle.chat.serverImpl;
 
-import com.alibaba.fastjson.JSON;
 import fz.idle.chat.entry.MessageDetail;
 import fz.idle.chat.entry.MetaData;
 import fz.idle.chat.enums.Types;
 import fz.idle.chat.param.LogParam;
-import fz.idle.chat.param.MsgParam;
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-import sun.jvm.hotspot.oops.Metadata;
-
-import java.io.PrintStream;
-import java.util.UUID;
-import java.util.concurrent.ForkJoinPool;
 
 /**
  * Hello world!
  */
-
 @Slf4j
 @RestController
 public class ChatConsumer {
